0ab969e11bdd3d6d9f93f0e2165557a5e11a8d98
tsarbucks <tiretracks@gmail.com>
2025-06-03 12:00:15 -0700

    update model id for inference capable models

--- lambda/app.py
+++ lambda/app.py
@@ -22,13 +22,13 @@ retriever = AmazonKendraRetriever(
 
 if(MODEL_TYPE == "CLAUDE"):
     llm = Bedrock(
-        model_id="anthropic.claude-sonnet-4-20250514-v1:0",
+        model_id="us.anthropic.claude-sonnet-4-20250514-v1:0",
         endpoint_url="https://bedrock-runtime." + REGION_NAME + ".amazonaws.com",
         model_kwargs={"temperature": 0.7, "max_tokens_to_sample": 4096}
     )
 
     condense_question_llm = Bedrock(
-        model_id="anthropic.claude-3-5-haiku-20241022-v1:0",
+        model_id="us.anthropic.claude-3-5-haiku-20241022-v1:0",
         endpoint_url="https://bedrock-runtime." + REGION_NAME + ".amazonaws.com",
         model_kwargs={"temperature": 0.7, "max_tokens_to_sample": 4096}
     )
